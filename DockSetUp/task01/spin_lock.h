ec46e4942738a2107b74d17febb6e52f971f4e80
Daniel Heilbron <134653973+TheValeyard02@users.noreply.github.com>
2025-05-06 15:45:48 +0300

    comments

--- DockSetUp/task01/spin_lock.h
+++ DockSetUp/task01/spin_lock.h
@@ -3,9 +3,16 @@ typedef atomic_flag spinlock;
 
 void spinlock_init(atomic_flag *lock);
 
+//--------------------------------------------------------------------\\
+
 void spinlock_acquire(atomic_flag *lock);
+
+//--------------------------------------------------------------------\\
 
 void spinlock_release(atomic_flag *lock);
 
+//--------------------------------------------------------------------\\
+
 void thread_func(void *arg);
 
+//----------------------------------End of File----------------------------------\\
