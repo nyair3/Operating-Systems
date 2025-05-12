a1988afeef39ea42ba2e66e8e406637608c29a82
nyair3 <nyair3@gmail.com>
2025-05-12 16:27:04 +0300

    Tasks 1,2,3 fully done :)

--- DockSetUp/task03/cond_var.c
+++ DockSetUp/task03/cond_var.c
@@ -1,33 +1,27 @@
 #include "cond_var.h"
 
-/*
- * TODO: Implement condition_variable_init.
- */
 void condition_variable_init(condition_variable *cv)
 {
     // TODO: Initialize internal fields.
     cv->waiters_count = 0;
     ticketlock_init(&cv->waiters_lock);
-    semaphore_init(&cv->sem, 0);
+    semaphore_init(&cv->sem, 0); 
 }
 //--------------------------------------------------------------------//
-/*
- * TODO: Implement condition_variable_wait.
- */
 void condition_variable_wait(condition_variable *cv, ticket_lock *ext_lock)
 {
     // TODO: Increase waiter count, release ext_lock, wait until signaled, then reacquire ext_lock.
-    ticketlock_acquire(&cv->waiters_lock);
+     ticketlock_acquire(&cv->waiters_lock);
     cv->waiters_count++;
     ticketlock_release(&cv->waiters_lock);
-
+    
     ticketlock_release(ext_lock);
-
+    
     semaphore_wait(&cv->sem);
-
+    
     ticketlock_acquire(ext_lock);
 }
-//--------------------------------------------------------------------//
+
 /*
  * TODO: Implement condition_variable_signal.
  */
@@ -35,30 +29,26 @@ void condition_variable_signal(condition_variable *cv)
 {
     // TODO: Signal one waiting thread.
     ticketlock_acquire(&cv->waiters_lock);
-    if (cv->waiters_count > 0)
-    {
-
+    if (cv->waiters_count > 0) {
+        
         cv->waiters_count--;
-        semaphore_signal(&cv->sem);
+        semaphore_signal(&cv->sem); 
     }
-    ticketlock_release(&cv->waiters_lock);
+    ticketlock_release(&cv->waiters_lock); //if no threads waiting release lock
 }
-//--------------------------------------------------------------------//
-/*
- * TODO: Implement condition_variable_broadcast.
- */
+
+//------------------------------------------------------------------------------//
+
 void condition_variable_broadcast(condition_variable *cv)
 {
-    // TODO: Signal all waiting threads.
-    ticketlock_acquire(&cv->waiters_lock);
+  ticketlock_acquire(&cv->waiters_lock);
     // Wake up all waiting threads
     int waiters = cv->waiters_count;
     for (int i = 0; i < waiters; i++)
     {
         semaphore_signal(&cv->sem);
     }
-    cv->waiters_count = 0;
-    ticketlock_release(&cv->waiters_lock);
+    cv->waiters_count = 0; //no waiting threads anymore, reset to 0
+    ticketlock_release(&cv->waiters_lock); 
 }
-//----------------------------------End of File----------------------------------//
-// gcc -o test03 ../task03/test03.c ../task03/cond_var.c ../task01/ticket_lock.c ../task01/tl_semaphore.c -lpthread
+//----------------------------------End of File----------------------------------//gcc -o test03 ../task03/test03.c ../task03/cond_var.c ../task01/ticket_lock.c ../task01/tl_semaphore.c -lpthread
--- DockSetUp/task03/test03.c
+++ DockSetUp/task03/test03.c
@@ -4,101 +4,75 @@
 #include "cond_var.h"
 
 #define THREADS 8
-#define SIGNAL_THREADS 3 // Number of threads to wake with individual signals
+#define SIGNAL_THREADS 3
 
 int counter = 0;
 ticket_lock lock;
 condition_variable cv;
 int threads_waiting = 0;
 
-// Thread function for waiter threads
 void *waiter_thread(void *arg)
 {
     int id = *(int *)arg;
-
-    // Acquire the lock
     ticketlock_acquire(&lock);
-
-    printf("Thread %d is waiting\n", id);
     threads_waiting++;
-
-    // Wait on the condition variable
-    condition_variable_wait(&cv, &lock);
-
-    // After waking up, increment the counter
-    counter++;
-    printf("Thread %d woke up, counter = %d\n", id, counter);
-
-    // Release the lock
-    ticketlock_release(&lock);
+    condition_variable_wait(&cv, &lock); // wait on condition var
+    counter++; // thread woke up, increment counter
+    ticketlock_release(&lock); // release lock
 
     return NULL;
 }
-//--------------------------------------------------------------------//
-// Thread function for the signaler
+
 void *signaler_thread(void *arg)
 {
-    // Sleep briefly to ensure waiter threads are waiting
-    usleep(100000); // 100ms
+    usleep(100000); // sleep --> 100ms
 
-    // Signal individual threads
     for (int i = 0; i < SIGNAL_THREADS; i++)
     {
-        printf("Signaling one thread\n");
         condition_variable_signal(&cv);
-        usleep(50000); // 50ms between signals
+        usleep(50000); // sleep --> 50ms
     }
 
-    // Sleep briefly before broadcast
-    usleep(100000); // 100ms
-
-    // Broadcast to wake up all remaining threads
-    printf("Broadcasting to all remaining threads\n");
-    condition_variable_broadcast(&cv);
+    usleep(100000); // sleep --> 100ms
+    condition_variable_broadcast(&cv); //wake up rest of waiting threads
 
     return NULL;
 }
 //--------------------------------------------------------------------//
 int main(void)
 {
-    // Initialize lock and condition variable
     ticketlock_init(&lock);
     condition_variable_init(&cv);
-
     pthread_t waiters[THREADS];
     pthread_t signaler;
     int ids[THREADS];
 
-    // Create waiter threads
     for (int i = 0; i < THREADS; i++)
-    {
+    { // create threads to wait
         ids[i] = i + 1;
         pthread_create(&waiters[i], NULL, waiter_thread, &ids[i]);
 
-        // Small delay to ensure threads start in order
-        usleep(10000); // 10ms
+        usleep(10000); // add delay --> 10ms
     }
 
-    // Create signaler thread
-    pthread_create(&signaler, NULL, signaler_thread, NULL);
-
-    // Join all threads
-    pthread_join(signaler, NULL);
+    pthread_create(&signaler, NULL, signaler_thread, NULL); // make threads to signal
+    pthread_join(signaler, NULL); // waits for signaler to terminate
+    
     for (int i = 0; i < THREADS; i++)
     {
-        pthread_join(waiters[i], NULL);
+        pthread_join(waiters[i], NULL); // waits for waiter threads to terminate
     }
 
-    // Verify results
     if (counter != THREADS)
     {
         printf("Value is %d, expected %d\n", counter, THREADS);
-        fprintf(stderr, "Failed!");
+        fprintf(stderr, "Failed!"); // error, we failed
     }
     else
     {
-        printf("Test passed! All %d threads woke up.\n", THREADS);
+        printf("All %d threads woke up :)\n", THREADS);
     }
+
 
     return 0;
 }
